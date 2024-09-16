9fdc6ac984db59b99f53a1bc9e6a0f8d8003f00b
Shashikant Chaubey <shashikantchaubey3@gmail.com>
2024-09-16 12:22:16 +0530

    first commit

---
+++ src/store/authSlice1.js
@@ -0,0 +1,28 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const initialState = {
+    status:false,
+    userData:null
+}
+
+const authSlice = createSlice({
+
+    name:'auth',
+    initialState,
+
+    reducer:{
+        login:(state, action) =>{
+            state.status = true,
+            state.userData = action.payload.userData;
+        },
+
+        logout:(state) => {
+            state.staus = false,
+            state.userData = null
+        }
+    }
+})
+
+export const {login, logout} = authSlice.actions;
+
+export default authSlice.reducer;
